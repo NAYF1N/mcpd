d53901c5cccdb50715a43229bb44c9eff3d35939
NAYF1N <NAYF1N@users.noreply.github.com>
2024-05-23 22:51:06 -0600

    Update noah.js

--- noah.js
+++ noah.js
@@ -1,37 +1,53 @@
 function update() {
 if (document.getElementById("check-aware").checked == false) {{{
-  document.querySelector("textarea.radar").value = `NOAH I checked the calibration of my radar unit at the beginning of my shift, and I perform an internal systems check of my radar system after each traffic stop.
+  document.querySelector("textarea.radar").value = `I performed an internal systems check at the beginning of my shift as I always do and it passed all tests.
 
-I observed a vehicle near ${locate.value} traveling ${observed.value}mph, and confirmed a speed of ${confirmed.value}mph in a ${zone.value}mph zone with my ${ms.value}, ${fr.value}, ${os.value} radar.
+I observed a vehicle near ${locate.value} traveling ${observed.value}mph, ${direction.value}and confirmed a speed of ${confirmed.value}mph in a ${zone.value}mph zone with my ${ms.value}, ${fr.value}, ${os.value} radar.
 
-I decided to issue a ${cw.value} for driving ${charged.value}mph in a ${zone.value}mph zone.`
+I decided to issue a ${cw.value} for driving ${charged.value}mph in a ${zone.value}mph zone.
+
+--- END OF COMMENTS.
+OFFICER N. GONZALEZ, 2T16
+MOAB CITY POLICE DEPARTMENT.`
   }
 ;
     {
-    document.querySelector("textarea.lidar").value = `NOAH The LiDAR passed an internal systems check when it powered on.
+    document.querySelector("textarea.lidar").value = `The LiDAR passed an internal systems check when it powered on.
 
-While sitting stationary, I observed a vehicle near ${locate.value} traveling ${observed.value}mph, in the ${os.value} lane relative to my position, and confirmed a speed of ${confirmed.value}mph in a ${zone.value}mph zone with my LiDAR.
+While sitting stationary, I observed a vehicle near ${locate.value} traveling ${observed.value}mph, ${direction.value}in the ${os.value} lane relative to my position, and confirmed a speed of ${confirmed.value}mph in a ${zone.value}mph zone with my LiDAR.
 
-I decided to issue a ${cw.value} for driving ${charged.value}mph in a ${zone.value}mph zone.`
+I decided to issue a ${cw.value} for driving ${charged.value}mph in a ${zone.value}mph zone.
+
+--- END OF COMMENTS.
+OFFICER N. GONZALEZ, 2T16
+MOAB CITY POLICE DEPARTMENT.`
   }}
 
 } else {{
-  document.querySelector("textarea.radar").value = `NOAH I checked the calibration of my radar unit at the beginning of my shift, and I perform an internal systems check of my radar system after each traffic stop.
+  document.querySelector("textarea.radar").value = `I checked the calibration of my radar unit at the beginning of my shift, and I perform an internal systems check of my radar system after each traffic stop.
 
-I observed a vehicle near ${locate.value} traveling ${observed.value}mph, and confirmed a speed of ${confirmed.value}mph in a ${zone.value}mph zone with my ${ms.value}, ${fr.value}, ${os.value} radar.
+I observed a vehicle near ${locate.value} traveling ${observed.value}mph, ${direction.value}and confirmed a speed of ${confirmed.value}mph in a ${zone.value}mph zone with my ${ms.value}, ${fr.value}, ${os.value} radar.
     
 When asked if the driver was aware of their speed and the speed limit, they stated they ${aspeed.value} of their speed, and ${alimit.value} of the speed limit.
 
-I decided to issue a ${cw.value} for driving ${charged.value}mph in a ${zone.value}mph zone.`
+I decided to issue a ${cw.value} for driving ${charged.value}mph in a ${zone.value}mph zone.
+
+--- END OF COMMENTS.
+OFFICER N. GONZALEZ, 2T16
+MOAB CITY POLICE DEPARTMENT.`
   }
 ;
     {
-    document.querySelector("textarea.lidar").value = `NOAH The LiDAR passed an internal systems check when it powered on.
+    document.querySelector("textarea.lidar").value = `The LiDAR passed an internal systems check when it powered on.
 
-While sitting stationary, I observed a vehicle near ${locate.value} traveling ${observed.value}mph, in the ${os.value} lane relative to my position, and confirmed a speed of ${confirmed.value}mph in a ${zone.value}mph zone with my LiDAR.
+While sitting stationary, I observed a vehicle near ${locate.value} traveling ${observed.value}mph, ${direction.value}in the ${os.value} lane relative to my position, and confirmed a speed of ${confirmed.value}mph in a ${zone.value}mph zone with my LiDAR.
     
 When asked if the driver was aware of their speed and the speed limit, they stated they ${aspeed.value} of their speed, and ${alimit.value} of the speed limit.
 
-I decided to issue a ${cw.value} for driving ${charged.value}mph in a ${zone.value}mph zone.`
+I decided to issue a ${cw.value} for driving ${charged.value}mph in a ${zone.value}mph zone.
+
+--- END OF COMMENTS.
+OFFICER N. GONZALEZ, 2T16
+MOAB CITY POLICE DEPARTMENT.`
   }}
 }
