882b1469d80dfb061742c800b8958f721a699be8
NAYF1N <NAYF1N@users.noreply.github.com>
2024-05-23 22:52:28 -0600

    Update schadie.js

--- schadie.js
+++ schadie.js
@@ -1,22 +1,22 @@
 function update() {
 if (document.getElementById("check-aware").checked == false) {{{
-  document.querySelector("textarea.radar").value = `SCHADIE I checked the calibration of my radar unit at the beginning of my shift, and I perform an internal systems check of my radar system after each traffic stop.
+  document.querySelector("textarea.radar").value = `I checked the calibration of my radar unit at the beginning of my shift, and I perform an internal systems check of my radar system after each traffic stop.
 
-I observed a vehicle near ${locate.value} traveling ${observed.value}mph, and confirmed a speed of ${confirmed.value}mph in a ${zone.value}mph zone with my ${ms.value}, ${fr.value}, ${os.value} radar.
+I observed a vehicle near ${locate.value} traveling ${observed.value}mph, ${direction.value}and confirmed a speed of ${confirmed.value}mph in a ${zone.value}mph zone with my ${ms.value}, ${fr.value}, ${os.value} radar.
 
 I decided to issue a ${cw.value} for driving ${charged.value}mph in a ${zone.value}mph zone.`
   }
 ;
     {
-    document.querySelector("textarea.lidar").value = `SCHADIE The LiDAR passed an internal systems check when it powered on.
+    document.querySelector("textarea.lidar").value = `The LiDAR passed an internal systems check when it powered on.
 
-While sitting stationary, I observed a vehicle near ${locate.value} traveling ${observed.value}mph, in the ${os.value} lane relative to my position, and confirmed a speed of ${confirmed.value}mph in a ${zone.value}mph zone with my LiDAR.
+While sitting stationary, I observed a vehicle near ${locate.value} traveling ${observed.value}mph, ${direction.value}in the ${os.value} lane relative to my position, and confirmed a speed of ${confirmed.value}mph in a ${zone.value}mph zone with my LiDAR.
 
 I decided to issue a ${cw.value} for driving ${charged.value}mph in a ${zone.value}mph zone.`
   }}
 
 } else {{
-  document.querySelector("textarea.radar").value = `SCHADIE I checked the calibration of my radar unit at the beginning of my shift, and I perform an internal systems check of my radar system after each traffic stop.
+  document.querySelector("textarea.radar").value = `I checked the calibration of my radar unit at the beginning of my shift, and I perform an internal systems check of my radar system after each traffic stop.
 
 I observed a vehicle near ${locate.value} traveling ${observed.value}mph, and confirmed a speed of ${confirmed.value}mph in a ${zone.value}mph zone with my ${ms.value}, ${fr.value}, ${os.value} radar.
     
@@ -26,7 +26,7 @@ I decided to issue a ${cw.value} for driving ${charged.value}mph in a ${zone.val
   }
 ;
     {
-    document.querySelector("textarea.lidar").value = `SCHADIE The LiDAR passed an internal systems check when it powered on.
+    document.querySelector("textarea.lidar").value = `The LiDAR passed an internal systems check when it powered on.
 
 While sitting stationary, I observed a vehicle near ${locate.value} traveling ${observed.value}mph, in the ${os.value} lane relative to my position, and confirmed a speed of ${confirmed.value}mph in a ${zone.value}mph zone with my LiDAR.
     
