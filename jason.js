99d0e68eca2bd4d8a79427721006b7b7bbe2aade
NAYF1N <NAYF1N@users.noreply.github.com>
2025-05-22 03:02:36 -0600

    Add files via upload

--- jason.js
+++ jason.js
@@ -1,14 +1,10 @@
 function update() {
 if (document.getElementById("check-aware").checked == false) {{{
-  document.querySelector("textarea.radar").value = `I performed an internal systems check at the beginning of my shift as I always do and it passed all tests.
+  document.querySelector("textarea.radar").value = `I performed an internal radar systems check at the beginning of my shift, and it passed all tests.
 
 I observed a vehicle near ${locate.value} traveling ${direction.value}${observed.value}mph, and confirmed a speed of ${confirmed.value}mph in a ${zone.value}mph zone with my ${ms.value}, ${fr.value}, ${os.value} radar.
 
-I decided to issue a ${cw.value} for driving ${charged.value}mph in a ${zone.value}mph zone.
-
---- END OF COMMENTS.
-TROOPER N. GONZALEZ, 734
-UTAH HIGHWAY PATROL`
+I decided to issue a ${cw.value} for driving ${charged.value}mph in a ${zone.value}mph zone.`
   }
 ;
     {
@@ -16,25 +12,17 @@ UTAH HIGHWAY PATROL`
 
 While sitting stationary, I observed a vehicle near ${locate.value} traveling ${direction.value}${observed.value}mph, in the ${os.value} relative to my position, and confirmed a speed of ${confirmed.value}mph in a ${zone.value}mph zone with my LiDAR.
 
-I decided to issue a ${cw.value} for driving ${charged.value}mph in a ${zone.value}mph zone.
-
---- END OF COMMENTS.
-TROOPER N. GONZALEZ, 734
-UTAH HIGHWAY PATROL`
+I decided to issue a ${cw.value} for driving ${charged.value}mph in a ${zone.value}mph zone.`
   }}
 
 } else {{
-  document.querySelector("textarea.radar").value = `I performed an internal systems check at the beginning of my shift as I always do and it passed all tests.
+  document.querySelector("textarea.radar").value = `I performed an internal radar systems check at the beginning of my shift, and it passed all tests.
 
 I observed a vehicle near ${locate.value} traveling ${direction.value}${observed.value}mph, and confirmed a speed of ${confirmed.value}mph in a ${zone.value}mph zone with my ${ms.value}, ${fr.value}, ${os.value} radar.
     
 When asked if the driver was aware of their speed and the speed limit, they stated they ${aspeed.value} of their speed, and ${alimit.value} of the speed limit.
 
-I decided to issue a ${cw.value} for driving ${charged.value}mph in a ${zone.value}mph zone.
-
---- END OF COMMENTS.
-TROOPER N. GONZALEZ, 734
-UTAH HIGHWAY PATROL`
+I decided to issue a ${cw.value} for driving ${charged.value}mph in a ${zone.value}mph zone.`
   }
 ;
     {
@@ -44,10 +32,6 @@ While sitting stationary, I observed a vehicle near ${locate.value} traveling ${
     
 When asked if the driver was aware of their speed and the speed limit, they stated they ${aspeed.value} of their speed, and ${alimit.value} of the speed limit.
 
-I decided to issue a ${cw.value} for driving ${charged.value}mph in a ${zone.value}mph zone.
-
---- END OF COMMENTS.
-TROOPER N. GONZALEZ, 734
-UTAH HIGHWAY PATROL`
+I decided to issue a ${cw.value} for driving ${charged.value}mph in a ${zone.value}mph zone.`
   }}
 }
